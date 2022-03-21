200c864241eeeef30f9ee08cbefb97080242a450
dimasma0305 <dimasmaulana0305@gmail.com>
2022-03-22 06:32:06 +0800

    some changes

--- Conky/myscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-deadbeef --nowplaying "%e %l" |
-awk '
-{ n = split("::" $1,t,":")
-  elapsed = (t[n-2]*60+t[n-1])*60+t[n]
-  n = split("::" $2,t,":")
-  total = (t[n-2]*60+t[n-1])*60+t[n]
-  printf "%d\n",elapsed*100/total
-}'
