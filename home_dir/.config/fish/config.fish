b7e31778290d22f1d8b69e576509e7428eb16fb6
dimas0305 <dimasmaulana0305@gmail.com>
2022-07-01 21:00:01 +0800

    Fri Jul  1 01:00:01 PM UTC 2022

---
+++ home_dir/.config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # My User binary PATH
+    set PATH "/home/wowon/Documents/My_Project/MyTools/bin" $PATH
+    set PATH /home/wowon/.local/bin $PATH
+    set PATH "/home/wowon/.local/share/gem/ruby/3.0.0/bin/" $PATH
+end
